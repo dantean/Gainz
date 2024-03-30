ce21430912489a8785b9c8513d679540a7f6e254
Joseph Monticelli <jdmonticelli@gmail.com>
2024-03-30 02:11:05 -0400

    homeRoutes.js progress

---
+++ controllers/api/homeRoutes.js
@@ -0,0 +1,85 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+//http://localhost:3001/
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const posts = post.Data.map((post) => post.get({ plain: true }));
+        console.log(posts)
+        res.render('homepage', {
+        posts,
+        logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+
+//http://localhost:3001/Post/5
+
+router.get('/post/:id', async (req, res) => {
+try {
+    const postData = await Post.findByPk(req.params.id, {
+        include: [
+            {
+                model: User,
+                attributes: ['username'],
+            },
+        ],
+    }),
+
+    const post =postData.get({ plain: true });
+        console.log(post)
+        res.render('post', {
+            ...post,
+            });
+
+const post = postData.get({ plain: true });
+console.log(post)
+res.render('post', {
+    ...post,
+    logged_in: req.session.logged_in    
+});
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+//http://localhost:3001/profile
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.friendByPk(req.session.user_id, {
+            attributes: { exclude: ['password']},
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+            console.log(user)
+            res.render('profile', {
+                ...user,
+                logged_in: true
+            });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+//http://localhost:3001/login
